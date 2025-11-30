1e2054895bdb8183e31928261f3fef18eb4c1557
SalmanHossen <hossainsalman873@gmail.com>
2025-11-30 14:17:49 +0600

    use props in bookrow

--- Step1-Practice Separately/1_First_Basics/src/BookRow.jsx
+++ Step1-Practice Separately/1_First_Basics/src/BookRow.jsx
@@ -1,8 +1,12 @@
+import BookDetails from "./BookDetails";
+import BookFeature from "./BookFeature";
 
-function BookRow(){
+
+function BookRow({book}){
    return(
       <div>
-
+         <BookDetails title={book.title} author={book.author}/>
+         <BookFeature/>
       </div>
    )
 }
--- Step1-Practice Separately/1_First_Basics/src/Booklist.jsx
+++ Step1-Practice Separately/1_First_Basics/src/Booklist.jsx
@@ -1,7 +1,28 @@
 
-function BookList(){
-   return(
-      <div></div>
+import BookRow from "./BookRow";
+
+const Books = [
+   { id: 1, title: "To Kill a Mockingbird", author: "Harper Lee", featured: true },
+   { id: 2, title: "1984", author: "George Orwell", featured: false },
+   { id: 3, title: "The Great Gatsby", author: "F. Scott Fitzgerald", featured: true },
+   { id: 4, title: "Pride and Prejudice", author: "Jane Austen", featured: false },
+   { id: 5, title: "The Catcher in the Rye", author: "J.D. Salinger", featured: false }
+];
+
+
+function BookList() {
+   return (
+      <div>
+         <ul>
+            {
+               Books.map((book)=>{
+                  <li key={book.id}>
+                     <BookRow book={book}/>
+                  </li>
+               })
+            }
+         </ul>
+      </div>
    )
 }
 export default BookList;
