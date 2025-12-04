fb223a21a4e5ae85d8c2c83d9da769d9ae2a7d48
SalmanHossen <hossainsalman873@gmail.com>
2025-12-04 14:40:11 +0600

    create Navbar

---
+++ Step1-Practice Separately/2.Props_Even_handling/3Ui_Design/src/components/section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-6 px-18 border-0'>
+      <h1 className='bg-black uppercase text-white px-6 py-2 rounded-full'>Target Audience</h1>
+      <button className='bg-gray-200 px-6 py-2 uppercase rounded-full tracking-wider text-sm'>Digital Banking Platform</button>
+    </div>
+  )
+}
+
+export default Navbar;
