80c11ccffe0ec24820d62ea9cbce89e31b0c0819
SalmanHossen <hossainsalman873@gmail.com>
2025-12-06 20:02:08 +0600

    practice localstorage set,get and remove

--- Step1-Practice Separately/5_Others/src/App.jsx
+++ Step1-Practice Separately/5_Others/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const App = () => {
+  //create 
+  localStorage.setItem('user','salman');
+  localStorage.setItem('age',18);
+
+  //get item
+  const user=localStorage.getItem('user');
+  const age=localStorage.getItem('age');
+  console.log(user,age);
+  
+  //remove 
+  localStorage.removeItem('age');
+  
   return (
     <div>
-      
+      <h1 className='text-center font-bold text-4xl p-4'>Practice Local storage</h1>
     </div>
   )
 }
