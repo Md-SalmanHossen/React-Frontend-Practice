88c1a19fc271227a812c4957e7a3629239583ec6
SalmanHossen <hossainsalman873@gmail.com>
2025-12-07 21:02:59 +0600

    create decrement btn

--- Step1-Practice Separately/3.React_Hooks/useState/src/App.jsx
+++ Step1-Practice Separately/3.React_Hooks/useState/src/App.jsx
@@ -10,10 +10,12 @@ const App = () => {
       setIncrement(prevValue => prevValue - 1);
    }
   return (
-    <div className='text-center'>
+    <div className='text-center m-10'>
       <h1 className='text-center text-3xl font-bold p-10 my-5'>Practice useState</h1>
 
-      <button className='text-center bg-green-500 cursor-pointer px-5 py-2 rounded-xl' onClick={IncrementValue}>Increment</button>
+      <button className='font-bolder text-center bg-green-500 cursor-pointer px-5 py-2 rounded-xl' onClick={IncrementValue}>Increment</button>
+      <button className='font-bolder text-center bg-red-500 cursor-pointer px-5 py-2 rounded-xl' onClick={decrementValue}>Increment</button>
+      <h1 className='text-center text-center'>{Increment}</h1>
       <h1 className='text-center text-center'>{Increment}</h1>
     </div>
   )
