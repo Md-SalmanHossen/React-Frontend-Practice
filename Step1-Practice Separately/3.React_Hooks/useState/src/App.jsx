a6ec29e4d9adb1c94965950dc1910345c98deba1
SalmanHossen <hossainsalman873@gmail.com>
2025-12-07 20:52:13 +0600

    create and setup all package

---
+++ Step1-Practice Separately/3.React_Hooks/useState/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center text-3xl font-bold p-10 my-5'>Practice useState</h1>
+    </div>
+  )
+}
+
+export default App
