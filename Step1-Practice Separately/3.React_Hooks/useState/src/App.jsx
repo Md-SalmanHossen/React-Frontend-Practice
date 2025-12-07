5f18cfe9d1aa9ee704bfb9a598737f6e202cd2c1
SalmanHossen <hossainsalman873@gmail.com>
2025-12-07 20:56:36 +0600

    create a increment valu

--- Step1-Practice Separately/3.React_Hooks/useState/src/App.jsx
+++ Step1-Practice Separately/3.React_Hooks/useState/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
+   const [Increment, setIncrement] = useState(0);
+
+   const IncrementValue=()=>{
+      setIncrement(prevValue => prevValue + 1);
+   }
   return (
     <div>
       <h1 className='text-center text-3xl font-bold p-10 my-5'>Practice useState</h1>
+
+      <button onClick={IncrementValue}>Increment</button>
+      <h1 className='text-center'>{Increment}</h1>
     </div>
   )
 }
