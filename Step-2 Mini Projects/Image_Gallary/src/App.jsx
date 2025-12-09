c25e1ef59bc3176b64e9af4facef3cce12417c29
SalmanHossen <hossainsalman873@gmail.com>
2025-12-09 14:25:58 +0600

    setup projects for image gallary

---
+++ Step-2 Mini Projects/Image_Gallary/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-4xl p-10'>Image Gallery Projects</h1>
+    </div>
+  )
+}
+
+export default App
