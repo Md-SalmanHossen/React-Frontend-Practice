313960c16810630ca3a15d7953941e982a3feaad
SalmanHossen <hossainsalman873@gmail.com>
2025-12-09 21:27:08 +0600

    practice basic routing

---
+++ Step1-Practice Separately/4.React_Router/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      
+      <div className="flex justify-between bg-gray-300 rounded-xl text-2xl font-medium p-4">
+        <h3>Salman</h3>
+        <div className=" flex justify-center items-center  gap-4 mx-4">
+          <Link to='/' className="hover:underline">Home</Link>
+          <Link to='/about' className="hover:underline">about</Link>
+          <Link to='/contact' className="hover:underline">contact</Link>
+          <Link to='/product' className="hover:underline">product</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
