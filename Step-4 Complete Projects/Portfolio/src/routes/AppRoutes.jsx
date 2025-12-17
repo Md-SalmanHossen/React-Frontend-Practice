21f24414ae3573d8ed5c93564832e2d86d5921e6
SalmanHossen <hossainsalman873@gmail.com>
2025-12-17 21:50:39 +0600

    create file and folder for projects

---
+++ Step-4 Complete Projects/Portfolio/src/routes/AppRoutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout'
+import HomePage from '../pages/HomePage'
+import AboutPage from '../pages/AboutPage'
+import ContactPage from '../pages/ContactPage'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout/>}/>
+      <Route index element={<HomePage/>}/>
+      <Route path='/about' element={<AboutPage/>}/>
+      <Route path='/contact' element={<ContactPage/>}/>
+    </Routes>
+  )
+}
+
+export default AppRoutes
