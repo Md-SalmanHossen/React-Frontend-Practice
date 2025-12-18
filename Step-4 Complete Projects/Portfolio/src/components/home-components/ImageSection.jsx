697efc2f85391405d715f81becdcf14f96293e5c
SalmanHossen <hossainsalman873@gmail.com>
2025-12-18 10:45:29 +0600

    create expertise section skeliton

--- Step-4 Complete Projects/Portfolio/src/components/home-components/ImageSection.jsx
+++ Step-4 Complete Projects/Portfolio/src/components/home-components/ImageSection.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ImageSection = () => {
   return (
-    <div className='w-1/2 mx-4'>
+    <div className='w-1/2 mx-4 flex justify-center items-center'>
       <img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMG1hbnxlbnwwfHwwfHx8MA%3D%3D" 
       alt="profile-img" 
-      className='size-125 flex justify-center items-center'/>
+      className='size-125 mr-2'/>
     </div>
   )
 }
