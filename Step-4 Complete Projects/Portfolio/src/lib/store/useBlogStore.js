71e736ab49ce04e001c6a236090ae5e45ec3d31a
SalmanHossen <hossainsalman873@gmail.com>
2025-12-22 22:53:21 +0600

    create blog store

---
+++ Step-4 Complete Projects/Portfolio/src/lib/store/useBlogStore.js
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+const useBlogStore=create((set)=>({
+   blogs:null,
+   getBlogsRequest: async()=>{
+      try {
+         let res=await 
+      } catch (error) {
+         
+      }
+   };
+}))
